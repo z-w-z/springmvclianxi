54e8101a14aebe3171d5a48c141390a5d069107d
z-w-z <mr_zwzheng@163.com>
2018-04-26 10:37:41 +0800

    springmvc测试

---
+++ src/main/java/com/zwz/ssm/controller/ItemsController1.java
@@ -0,0 +1,17 @@
+package com.zwz.ssm.controller;
+
+
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.annotation.Annotation;
+
+public class ItemsController1  implements Controller {
+
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        return null;
+    }
+}
